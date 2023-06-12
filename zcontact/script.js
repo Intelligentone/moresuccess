def7f175923a46b787f2551eb98bc2e2d0330054
Intelligentone <122197955+Intelligentone@users.noreply.github.com>
2023-06-11 18:05:47 -0700

    Update script.js

--- zcontact/script.js
+++ zcontact/script.js
@@ -6,24 +6,21 @@ const errorElement = document.getElementById('error')
 form.addEventListener('submit', (e) => {
   let messages = []
   if (name.value === '' || name.value == null) {
-    messages.push('Fill out this field')
+    messages.push('Name is required')
   }
 
-  if (email.value.length <= 6) {
-    messages.push('Email must be longer than 6 characters')
+  if (email.value.length <= 0) {
+    messages.push('Password must be longer than 0 characters')
   }
 
   if (email.value.length >= 20) {
-    messages.push('Email must be less than 20 characters')
+    messages.push('Password must be less than 20 characters')
   }
 
-  if (email.value === 'email') {
-    messages.push('Enter an email')
+  if (email.value === 'password') {
+    messages.push('Password cannot be password')
   }
-  
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-  
-    if (messages.length > 0) {
+  if (messages.length > 0) {
     e.preventDefault()
     errorElement.innerText = messages.join(', ')
   }
