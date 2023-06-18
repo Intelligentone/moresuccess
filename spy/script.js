96583b3b50b4985b861fe7ff0eba758fc933286e
Intelligentone <122197955+Intelligentone@users.noreply.github.com>
2023-06-18 03:08:51 -0700

    Update script.js

--- spy/script.js
+++ spy/script.js
@@ -1,1 +1,27 @@
+var Pig = document.getElementById("pig");
+var Sun = document.getElementById("sun");
+var Tree = document.getElementById("tree");
 
+function foundPig(){
+  document.getElementById("item1").style.textDecoration="line-through";
+}
+
+Pig.addEventListener("click", function(){
+  foundPig("Pig");
+}, false);
+
+function foundSun(){
+  document.getElementById("item2").style.textDecoration="line-through";
+}
+
+Sun.addEventListener("click", function(){
+  foundSun("Sun");
+}, false);
+
+function foundTree(){
+  document.getElementById("item3").style.textDecoration="line-through";
+}
+
+Tree.addEventListener("click", function(){
+  foundTree("Tree");
+}, false);
