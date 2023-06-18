8a7ae4a3bb0ebb14d743a51671cff4288e925428
Intelligentone <122197955+Intelligentone@users.noreply.github.com>
2023-06-18 02:49:13 -0700

    Update script.js

--- xispy/script.js
+++ xispy/script.js
@@ -1,1 +1,27 @@
+var box = document.getElementById("box");
+var ten = document.getElementById("ten");
+var k = document.getElementById("kay");
 
+function foundBox(){
+  document.getElementById("item1").style.textDecoration="line-through";
+}
+
+Box.addEventListener("click", function(){
+  foundBox("box");
+}, false);
+
+function foundTen(){
+  document.getElementById("item2").style.textDecoration="line-through";
+}
+
+Ten.addEventListener("click", function(){
+  foundTen("ten");
+}, false);
+
+function foundKay(){
+  document.getElementById("item3").style.textDecoration="line-through";
+}
+
+Kay.addEventListener("click", function(){
+  foundTree("Kay");
+}, false);
